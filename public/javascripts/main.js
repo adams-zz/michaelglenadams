0afa8e78bb3b5b4bbe85660fc639230512653415
Mike Adams <michaelglenadams@gmail.com>
2013-04-04 13:14:06 -0700

    minor tweaks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,2 +1,10 @@
-console.log("hey!")
-$('span.man-content').hide()
+$(document).ready(function(){
+    $('.box').mouseenter(function(){
+        // $(this).addClass('greyed')
+        $(this).children().show("easing");
+    })
+    $('.box').mouseleave(function(){
+        // $(this).removeClass('greyed')
+        $(this).children().hide("easing");
+    })
+})
